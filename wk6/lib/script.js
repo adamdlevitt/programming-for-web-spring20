992680117359136e75974152cac4d39b95ffbc20
Adam Levitt <adamdlevitt@gmail.com>
2020-03-05 19:41:49 -0600

    wk7 exercise 7.1 initial commit

--- wk6/lib/script.js
+++ wk6/lib/script.js
@@ -1,15 +1,6 @@
 new Vue({
-    el: "#llamaFacts",
+    el: "#llamaAlpaca",
     data: {
-      llamaFamily: "Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels.!",
-      llamaDomestic: "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands.",
-      llamaHeight: "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall.",
-      llamaWeight: "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight, so a 400-pound male llama can carry about 100 to 120 pounds on a trek of 10 to 12 miles with no problem.",
-      llamaLimit: "Llamas know their own limits. If you try to overload a llama with too much weight, the llama is likely to lie down or simply refuse to move.",
-      llamaHardy: "Llamas are hardy and well suited to harsh environments. They are quite sure-footed, easily navigating rocky terrain at high altitudes.",
-      llamaGuard: "Llamas have been used as guard animals for livestock like sheep or even alpacas in North America since the '80s. They require almost no training to be an effective guard.",
-      llamaSpit: "Llamas don't bite. They spit when they're agitated, but that's mostly at each other. Llamas also kick and neck wrestle each other when agitated.",
-      llamaDiet: "Llamas are vegetarians and have very efficient digestive systems.",
-      llamaColor: "Llamas come in a range of solid and spotted colors including black, gray, beige, brown, red, and white."
+      
     }
   });
---
+++ wk7/exercise7.1/lib/script.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#llamaAlpaca",
+    data: {
+        pageHeader: {
+            color: 'red',
+            fontSize: '20px'
+        },
+        classObject: {
+            active: true
+        },
+        llamaWiki: "https://en.wikipedia.org/wiki/Llama",
+        alpacaWiki: "https://en.wikipedia.org/wiki/Alpaca"
+    }
+  });
