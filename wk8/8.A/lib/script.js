4ebf171464b825bc5f5b687e78d2b6743e922dd3
Adam Levitt <adamdlevitt@gmail.com>
2020-04-04 11:20:26 -0500

    revisions to 8.A

--- wk8/8.A/lib/script.js
+++ wk8/8.A/lib/script.js
@@ -1,50 +1,78 @@
-const vm = new Vue({
+var vm = new Vue({
     el: '#app',
     data: {
-        consoles: [
+        newComicObj: {
+            title: '',
+            author: '',
+            publisher: '',
+            genre: '',
+            summary: '',
+            collected: false
+        },
+        comics: [
             {
-                manufactuer: 'Atari',
-                name: '2600',
-                releaseDate: '09/1977',
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Atari-2600-Wood-4Sw-Set.jpg/320px-Atari-2600-Wood-4Sw-Set.jpg',
-                generation: '2nd',
-                collected: false
-            }, {
-                manufactuer: 'Atari',
-                name: '5200',
-                releaseDate: '11/1982',
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Atari-5200-4-Port-wController-L.jpg/320px-Atari-5200-4-Port-wController-L.jpg',
-                generation: '2nd',
-                collected: false
-            }, {
-                manufactuer: 'Coleco',
-                name: '(Coleco) Vision',
-                releaseDate: '08/1982',
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Atari-5200-4-Port-wController-L.jpg/320px-Atari-5200-4-Port-wController-L.jpg',
-                generation: '2nd',
-                collected: false
-            }, {
-                manufactuer: 'Nintendo',
-                name: '(Nintendo) Entertainment System',
-                releaseDate: '07/1985',
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/320px-NES-Console-Set.jpg',
-                generation: '3rd',
-                collected: true
-            }, {
-                manufactuer: 'Nintendo',
-                name: 'Super Nintendo Entertainment System',
-                releaseDate: '11/1990',
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/SNES-Mod1-Console-Set.jpg/320px-SNES-Mod1-Console-Set.jpg',
-                generation: '4th',
-                collected: true
-            }, {
-                manufactuer: 'Sega',
-                name: 'Dreamcast',
-                releaseDate: '11/1998',
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Dreamcast-Console-Set.jpg/320px-Dreamcast-Console-Set.jpg',
-                generation: '6th',
-                collected: true
+            title: 'Cankor',
+            author: 'Matthew Allison',
+            publisher: 'AdHouse Books',
+            genre: 'Alternative/Psychedelic',
+            summary: 'There is drama, action, heroics and adversarial confrontations, - Cankor breaks rules, sets new standards and stays weirdly fun through and through.',
+            collected: false
+            },
+            {
+            title: 'COPRA',
+            author: 'Michel Fiffe',
+            publisher: 'Image comics',
+            genre: 'Action/Espionage',
+            summary: 'They are ugly. They are mean. But up until today, they have always been loyal. So when one of their own betrays them, COPRA has no choice but to turn their nightmarish skills back on everyone who ever even looked at them funny.',
+            collected: true
+            },
+            {
+            title: 'Zerosis Deathscape',
+            author: 'Simmons & Bayer',
+            publisher: 'All-Time Comics',
+            genre: 'Outlaw',
+            summary: 'Smashing together all the characters established in Season 1 of All-Time Comics, space and time meet anti-time and alternate-space, pushing them into new realms of inter-dimensional ferocity, and excitement!',
+            collected: true
+            },
+            {
+            title: 'Saga',
+            author: 'Brian K. Vaughan & Fiona Staples',
+            publisher: 'Image Comics',
+            genre: 'Epic Space Opera/Fantasy',
+            summary: 'Husband and wife, Alana and Marko, from long-warring extraterrestrial races, flee authorities from both sides of a galactic war as they struggle to care for their daughter, Hazel.',
+            collected: true
+            },
+            {
+            title: 'Immortal Hulk',
+            author: 'Joe Bennett & Al Ewing',
+            publisher: 'Marvel',
+            genre: 'Superhero/Horror',
+            summary: 'The drastic shifts in the Hulk character are terrifying elements of horror, religious symbolism, character pacing tangential to upheavals, almost.  Having said that, The Immortal Hulk performs amazing at each level. Forget the angry giant that punched into things, The Immortal Hulk is a fantastic horror comic that may have given a new life to an otherwise vanilla character.',
+            collected: false
             }
         ]
+    },
+    methods: {
+        submitHandler: () => {
+            console.log('submitted', vm.comics);
+            vm.comics = vm.comics.concat(vm.newComicObj);
+            vm.resetForm();
+        },
+        resetForm: () => {
+            vm.newComicObj = {
+                comic: '',
+                author: '',
+                publisher: '',
+                genre: '',
+                summary: '',
+                collected: false
+            };
+        },
+        deleteItem: item => {
+            vm.comics = vm.comics.filter(comic => {
+                return comic !== item;
+            })
+        }
     }
-});
+}
+);
